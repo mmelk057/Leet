ce9bb1176c41a2f8d48decc6acf02820e2e2c00b
Maxim Melkonian <mmelk057@uottawa.ca>
2019-06-29 09:56:17 -0400

    #25: LeetCode question #521; longest uncommon char subsequence

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Q520.detectCapitalUse("FlaG"));
+        System.out.println(Q521.findLUSlength("abbbaccd","abbbaccd"));
     }
 }
---
+++ src/Q521.java
@@ -0,0 +1,8 @@
+public class Q521 {
+    public static int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        }
+        return a.length() > b.length() ? a.length() : b.length();
+    }
+}
